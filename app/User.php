210427e19cf81bae00972d33f0bea7c102ab8dd6
smkorde <sudhir_korde@infosys.com>
2020-08-05 20:30:14 +0530

    Update User.php

--- app/User.php
+++ app/User.php
@@ -29,4 +29,9 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     protected $hidden = [
         'password',
     ];
+    function fun($a) {
+  $i = 10;
+  return $i + $a;
+  $i++;             // dead code
+}
 }
